d8a69b8747bf321c2bdb2c34753667c169c56464
Karla Soto <Karla_3soto@hotmail.com>
2023-12-06 14:22:33 -0800

    Initial Commit

---
+++ LocalLikeYou/LocalLikeYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalLikeYouApp.swift
+//  LocalLikeYou
+//
+//  Created by Karla Soto on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct LocalLikeYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
